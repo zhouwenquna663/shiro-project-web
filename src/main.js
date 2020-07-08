f2ff1e0dde6f86ca775b8d38795b5ad95cab6561
zhouwenquan <1471754591@qq.com>
2020-07-08 10:12:19 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,41 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const originalPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return originalPush.call(this, location).catch(err => err)
+}
+
+Vue.use(ElementUI);
+Vue.prototype.$http = axios
+Vue.prototype.$http.defaults.baseURL = 'http://localhost:8808'
 
 Vue.config.productionTip = false
+
+axios.interceptors.request.use(function (config) {
+  // console.log(config)
+  if (localStorage.token) {
+    config.headers.Authorization = localStorage.token;
+  }
+  // 在发送请求之前做些什么
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,41 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: "login",
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/index',
+    redirect: "home",
+    name: 'index',
+    component: () => import(/* webpackChunkName: "login" */ '../views/index/index.vue'),
+    children:[
+      {
+        path: '/home',
+        name: 'home',
+        component: () => import(/* webpackChunkName: "login" */ '../views/home/home.vue'),
+      },{
+        path: '/user',
+        name: 'user',
+        component: () => import(/* webpackChunkName: "user" */ '../views/user/user.vue')
+      }
+    ]
+  }, {
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/login/login.vue')
+  },{
+    path: '/test',
+    name: 'test',
+    component: () => import(/* webpackChunkName: "test" */ '../views/test/test.vue')
+  },{
+    path: '/home',
+    name: 'home',
+    component: () => import(/* webpackChunkName: "home" */ '../views/home/home.vue')
   }
 ]
 
